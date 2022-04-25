1dbe8e0376897280a65c914ebdc260a0b12d03ae
Gianluca Cavajani <cavajanig@gmail.com>
2022-04-25 18:06:22 -0300

    ex 4.16

--- controllers/users.js
+++ controllers/users.js
@@ -6,6 +6,14 @@ const User = require('../models/user')
 usersRouter.post('/api/users', async (req,res)=>{
     const {password, name, username} = req.body
     
+    const existingUser = await User.findOne({username})
+    if(existingUser){
+        return res.status(400).json({error: 'username must be unique'})
+    }
+    if(username.length <= 3 || password.length <= 3){
+        return res.status(400).json({error: 'password and username must be longer than 3 characters'})
+    }
+
     const passwordHash = await bcrypt.hash(password,10)
     const user = new User({
         name,username,passwordHash
